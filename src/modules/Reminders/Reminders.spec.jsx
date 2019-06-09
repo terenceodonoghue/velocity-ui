6b307357aa5aee60e02e73f3a2664d16d9e82fcf
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2019-06-09 12:31:48 +1000

    Update tests

--- src/modules/Reminders/Reminders.spec.jsx
+++ src/modules/Reminders/Reminders.spec.jsx
@@ -28,6 +28,7 @@ describe('Reminders', () => {
 
       expect(component.find(S.Column).at(0).props()['data-role']).toBe('service-needed');
       expect(component.find(S.Heading).at(0).text()).toBe('Service needed');
+      expect(component.find(S.Heading).at(0).props().count).toBe(props.data[0].length);
     });
 
     it('should render second column', () => {
@@ -37,6 +38,7 @@ describe('Reminders', () => {
 
       expect(component.find(S.Column).at(1).props()['data-role']).toBe('waiting');
       expect(component.find(S.Heading).at(1).text()).toBe('Waiting');
+      expect(component.find(S.Heading).at(1).props().count).toBe(props.data[1].length);
     });
 
     it('should render third column', () => {
@@ -46,6 +48,7 @@ describe('Reminders', () => {
 
       expect(component.find(S.Column).at(2).props()['data-role']).toBe('in-service');
       expect(component.find(S.Heading).at(2).text()).toBe('In service');
+      expect(component.find(S.Heading).at(2).props().count).toBe(props.data[2].length);
     });
 
     it('should render fourth column', () => {
@@ -55,6 +58,7 @@ describe('Reminders', () => {
 
       expect(component.find(S.Column).at(3).props()['data-role']).toBe('fully-serviced');
       expect(component.find(S.Heading).at(3).text()).toBe('Fully serviced');
+      expect(component.find(S.Heading).at(3).props().count).toBe(props.data[3].length);
     });
   });
 });

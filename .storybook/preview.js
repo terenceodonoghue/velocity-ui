9bd55ecb1007089881ed78e3fa77018a285fa8a0
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2020-01-19 21:29:40 +1100

    Fix decorator CSS

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import { css } from '@emotion/core';
+/** @jsx jsx */
+import { css, jsx } from '@emotion/core';
 import { addDecorator } from '@storybook/react';
 import { Global, ThemeProvider } from '../src/components';
 
 addDecorator((storyFn) => (
   <div
     css={css`
-      position: 'relative';
-      max-width: '1360px';
-      padding: '110px';
+      position: relative;
+      max-width: 1360px;
+      padding: 110px;
     `}
   >
     <ThemeProvider>

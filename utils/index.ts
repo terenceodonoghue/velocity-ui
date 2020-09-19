610c4ad5c405731d43285a83f490f758d4c13b53
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2020-09-19 14:27:37 +1000

    Add facepaint

--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,2 @@
-/* eslint-disable import/prefer-default-export */
 export { default as hexRgb } from './hex-rgb';
+export { default as mq } from './mq';
---
+++ utils/mq.ts
@@ -0,0 +1,6 @@
+import facepaint from 'facepaint';
+
+export default facepaint([
+  '@media(min-width: 770px)',
+  '@media(min-width: 1360px)',
+]);
